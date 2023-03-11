0fb23050515e8b7420cd37c2ec0511cb8bd9ca86
Daniel Mateu <danielmateu86@gmail.com>
2023-03-11 23:43:04 +0100

    Creating the conexions to the api and far away!

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const Banner = () => {
     return (
-        <div>Banner</div>
+        <div>
+            {/* <div>
+                <Image
+
+                />
+            </div> */}
+        </div>
     )
 }
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps = async (ctx) => {
+
+    
+
+    return {
+        props: {
+
+        }
+    }
+}
+
